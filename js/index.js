e4ec4c8af55e16185eed7514bbf5011c95855bee
Ashen-Q <dylanhays849@gmail.com>
2024-12-19 18:44:16 -0500

    The three main errors I found were SyntaxError, ReferenceError, & TypeError.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let three = 3;
+console.log(three);
+*/
+
+/*
+let blue = 2;
+console.log(blue);
+*/
+
+/*
+const color = "red";
+console.log(color);
+*/
+
+/*
+let green = 2;
+console.log(two);
+//Uncaught ReferenceError: two is not defined
+*/
+
+/*
+console.log(favoriteFood);
+let favoriteFood = "Pizza";
+//Uncaught ReferenceError: can't access lexical declaration 'favoriteFood' before initialization
+*/
+
+/*
+let class = "Science";
+console.log(Science);
+// SyntaxError: "class" is a reserved keyword in JavaScript
+*/
+
+/*
+const city = "Cincinnati";
+console.log(city);
+city = "Louisville";
+//Uncaught TypeError: invalid assignment to const 'city'
+*/
